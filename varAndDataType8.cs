a753363c51cfbce1efbc3059694dce7b94f288c6
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:52:19 +0200

    Create varAndDataType8.cs
    Create a variable to hold an average score.
    Assign a 67.5 to your variable.
    Print variables to Console.

---
+++ varAndDataType8.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double avgScore = 67.5;
+            Console.WriteLine(avgScore);
+        }
+    }
+}
