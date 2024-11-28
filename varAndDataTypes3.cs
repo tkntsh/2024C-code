32b2fbb1538dc7a203fd754bab3de97c2af567e1
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:45:31 +0200

    Create varAndDataTypes3.cs
    Create a variable to hold money out.
    Assign a 455.20 to your variable.
    Print your variable to the Console.

---
+++ varAndDataTypes3.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyOut = 455.20;
+            Console.WriteLine(moneyOut);
+        }
+    }
+}
