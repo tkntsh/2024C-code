458cc9e16012f91e1da68268a39e5a6ba13a9b94
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:51:31 +0200

    Create varAndDataType7.cs
    Create a variable to hold your home town.
    Assign a value to your variable.
    Print variables to Console.

---
+++ varAndDataType7.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string homeTown = "Amanzimtoti";
+            Console.WriteLine(homeTown);
+        }
+    }
+}
