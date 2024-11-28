cf86fe13b5bf734d566117ce371803cfbc6df2b2
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:53:18 +0200

    Create varAndDataType9.cs
    Create two variables, x and y.
    Assign x to true.
    Assign y to false.
    Print true to the Console.

---
+++ varAndDataType9.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "true";
+            string y = "false";
+            Console.WriteLine(x);
+        }
+    }
+}
