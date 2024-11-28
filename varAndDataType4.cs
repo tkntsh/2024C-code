9e2d6e4d5d26f20d17c73e4ff6a12111d00946d5
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:47:25 +0200

    Create varAndDataType4.cs
    Create a variable named date.
    Assign 12 to date.
    Print date to the Console.

---
+++ varAndDataType4.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int date = 12;
+            Console.WriteLine(date);
+        }
+    }
+}
