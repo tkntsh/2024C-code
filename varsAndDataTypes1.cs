59a188414df088aa91aa6d75c5c8733e8f23c9f9
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:39:42 +0200

    Create varsAndDataTypes1.cs
    Create a variable named age.
    Assign a value to your variable.
    Print your variable to the Console.

---
+++ varsAndDataTypes1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace app3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 12;
+            Console.WriteLine("Your age is: " + age);
+        }
+    }
+}
