23e3e04c11ae7df985926db04a901a522e14e3b7
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 15:41:04 +0200

    Create varAndDataTypes2.cs
    Create a variable to hold your telephone number.
    Assign 0123456789 to your variable.
    Print your variable to the Console.

---
+++ varAndDataTypes2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string telephoneNum = "0123456789";
+            Console.WriteLine(telephoneNum);
+        }
+    }
+}
